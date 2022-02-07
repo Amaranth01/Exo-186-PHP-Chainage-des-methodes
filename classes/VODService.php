c410c63ec8f8310249b99e3bac17e5c517a8b17d
Amaranth01 <vanessa.nectoux@gmail.com>
2022-02-07 14:40:36 +0100

    Exo 186 ok

---
+++ classes/VODService.php
@@ -0,0 +1,41 @@
+<?php
+
+class VODService {
+    protected string $name;
+    protected float $price;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): VODService
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): VODService
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+}
